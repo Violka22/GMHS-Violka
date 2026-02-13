8a479e4cde2300fcb534806daa6fddbe76db8d4c
Violka22 <violka.lukasova@seznam.cz>
2026-02-13 10:55:49 +0100

    13.2. 2026

---
+++ 3. lekce - úkol/script.js
@@ -0,0 +1,27 @@
+alert("zkouška");
+alert(3);
+
+// proměnné pro jména
+
+let userName = "Violka";
+console.log(userName);
+
+userNameL = "Johanka";
+console.log(userName);
+
+// proměnné pro věk obou lidí
+
+let ageV = 17;
+console.log(ageV)
+let ageJ = 16;
+console.log(ageJ)
+
+// součet věků
+
+let soucet = ageJ + ageV;
+console.log(soucet)
+
+// věta s oběma jmény a součtem
+
+let text = usernameL + " a " + username + " mají " + soucet + " let."
+console.log (text)
