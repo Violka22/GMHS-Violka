166bb2cbc1571430ab597a06b62b1ce8716377f1
Violka22 <violka.lukasova@seznam.cz>
2026-04-14 14:19:32 +0200

    14.4.2026

--- 5. lekce - úkol/script.js
+++ 5. lekce - úkol/script.js
@@ -1,1 +1,26 @@
-let age = 3
+let age = 30;
+let isTuesday = false;
+let hasLoyaltyCard = true;
+
+let price;
+
+if (age < 3) {
+    price = 0;
+} else if (isTuesday) {
+    price = 5;
+} else if (age <= 12) {
+    price = 5;
+} else if (age <= 17) {
+    price = 7;
+} else if (age <= 64) {
+    price = 10;
+} else {
+    price = 6;
+}
+
+//sleva věrnostní kartou
+if (price < 0) {
+    price = 0;
+}
+
+console.log("cena lístku je $ " + price);
